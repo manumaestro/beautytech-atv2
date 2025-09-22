022f0f7796bb65b6a696d7dbf65c8d7416ff0153
manumaestro <manuela.maestro@aluno.senai.br>
2025-09-22 10:50:40 -0300

    feat: Remoções com delete, limpeza de estoque e descontinuação de linha

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 -- Primeira Parte: Inserção de Dados; Populate do catálogo manual e do catálogo com IA
+
+
+-- Segunda Parte: Update (Atualizações) - Black Friday, ajuste de preços por marca 
+
---
+++ atividade/03-delete.sql
@@ -0,0 +1,13 @@
+-- Produtos com estoque < 10
+DELETE FROM produtos
+WHERE estoque < 10;
+
+-- Produtos com preço < R$20
+DELETE FROM produtos < R$20 
+DELETE FROM produtos 
+WHERE preco < 20;
+
+
+-- Descontinuação de linha
+DELETE FROM produtos 
+WHERE marca = 'Urban Decay';
